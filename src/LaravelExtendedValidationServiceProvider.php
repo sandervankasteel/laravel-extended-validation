376ee53e7953ffd94118231651d3e64df9399281
Sander van Kasteel <info@sandervankasteel.nl>
2021-05-22 14:47:06 +0200

    Added initial ServiceProvider

---
+++ src/LaravelExtendedValidationServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+namespace SandervanKasteel\LaravelExtendedValidation;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelExtendedValidationServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+}
