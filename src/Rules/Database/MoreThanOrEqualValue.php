57dbe4d56d70a5eff4d5148fc38824b5a9318802
Sander van Kasteel <info@sandervankasteel.nl>
2021-11-28 13:10:44 +0100

    Added MoreThanValue and MoreThanOrEqualValue database rules with tests

---
+++ src/Rules/Database/MoreThanOrEqualValue.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace SandervanKasteel\LaravelExtendedValidation\Rules\Database;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\DB;
+
+class MoreThanOrEqualValue implements Rule
+{
+    private $column;
+
+    private $table;
+
+    private $identifierColumn;
+
+    private $uniqueIdentifier;
+
+    private $foundValue;
+
+    public function __construct($table, $column, $identifierColumn, $uniqueIdentifier)
+    {
+        $this->table = $table;
+        $this->column = $column;
+
+        $this->identifierColumn = $identifierColumn;
+        $this->uniqueIdentifier = $uniqueIdentifier;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function passes($attribute, $value)
+    {
+        $res = DB::table($this->table)
+            ->where($this->identifierColumn, $this->uniqueIdentifier)
+            ->get($this->column)
+            ->first();
+
+        if($res === null) {
+            return false;
+        }
+
+        $this->foundValue = $res->{$this->column};
+
+        return $value >= $this->foundValue;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function message()
+    {
+        return "$this->column can not be more then $this->foundValue";
+    }
+}
