b84864692fe834f6d653321cbf451eda6b5b44b0
Sander van Kasteel <info@sandervankasteel.nl>
2021-11-30 22:25:28 +0100

    Added a validation rule for IBAN numbers

---
+++ src/Rules/Payment/IBAN.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace SandervanKasteel\LaravelExtendedValidation\Rules\Payment;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Str;
+
+class IBAN implements Rule
+{
+    public function passes($attribute, $value): bool
+    {
+        $countryCodeRegex = '/(^[A-Z][A-Z][0-9][0-9])/';
+
+        $value = (string) Str::of($value)
+            ->upper()
+            ->replace(' ', '')
+            ->replaceMatches($countryCodeRegex, '')
+            ->append(Str::of($value)->match($countryCodeRegex))
+            ->replaceMatches('/^([A-Z])([A-Z])([A-Z])([A-Z])/', function ($matches) {
+                return $this->transposeLettersToNumbers($matches);
+            })
+            ->replaceMatches('/([A-Z])/', function ($matches) {
+                return $this->transposeLettersToNumbers($matches);
+            });
+
+        return bcmod($value, '97') === "1";
+    }
+
+    public function message()
+    {
+
+    }
+
+    private function transposeLettersToNumbers(array $matches): string
+    {
+        $alphabet = range('A', 'Z');
+        $replacedValues = "";
+
+        for ($i = 1; $i < count ($matches); $i++) {
+            $match = $matches[$i];
+            $replacedValues .= (int)array_search($match, $alphabet) + 10;
+        }
+
+        return $replacedValues;
+    }
+}
---
+++ tests/Payment/IBANTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use SandervanKasteel\LaravelExtendedValidation\Rules\Payment\IBAN;
+
+test('that we can successfully validate an IBAN number', function ($ibanNumber, $valid) {
+    $sut = new IBAN();
+
+    expect(
+        $sut->passes('some-attribute', $ibanNumber)
+    )->toBe($valid);
+
+})->with([
+    [ 'GB82 WEST 1234 5698 7654 32', true ],
+    [ 'NL69INGB0123456789', true ],
+    [ 'NL32RABO1234567890', false ],
+]);
+
