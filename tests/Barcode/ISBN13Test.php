b0c2e954edd38a339bfce812b6b78cf939a138dd
Sander van Kasteel <info@sandervankasteel.nl>
2021-12-11 13:41:42 +0100

    Expanded ISBN13 tests to also include codes that are shorter and longer then 13 characters

--- tests/Barcode/ISBN13Test.php
+++ tests/Barcode/ISBN13Test.php
@@ -13,6 +13,8 @@ test('That we can successfully validate ', function ($isbn, $expectedValue) {
     ['978_0_306_40615_7', true],
     ['9789044637311', true],
     ['978-0-306-40615-8', false],
+    ['0306406158', false],
+    ['97803064061589', false],
 ]);
 
 test('that the attribute is being returned in the error message', function () {
