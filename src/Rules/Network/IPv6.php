f3d82d0a0ec3aaf86a80f1fc14411d8c0211925b
Sander van Kasteel <info@sandervankasteel.nl>
2021-12-19 00:48:28 +0100

    Fixed code style

--- src/Rules/Network/IPv6.php
+++ src/Rules/Network/IPv6.php
@@ -6,14 +6,13 @@ use Illuminate\Support\Str;
 
 class IPv6 implements \Illuminate\Contracts\Validation\Rule
 {
-
     /**
      * @inheritDoc
      */
     public function passes($attribute, $value): bool
     {
         // Based on this Stackoverflow answer: https://stackoverflow.com/a/5567938
-        return preg_match("/^([0-9A-Fa-f]{0,4}:){2,7}([0-9A-Fa-f]{1,4}$|".
+        return preg_match('/^([0-9A-Fa-f]{0,4}:){2,7}([0-9A-Fa-f]{1,4}$|'.
             "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4})$/",
             $value
         );
--- tests/Network/IPv6Test.php
+++ tests/Network/IPv6Test.php
@@ -8,16 +8,15 @@ test('that we can validate an IPv6 address', function ($ipAddress, $expectedOutu
     expect(
         $sut->passes('some-attribute', $ipAddress)
     )->toBe($expectedOutut);
-
 })->with([
-    [ '2001:0db8:85a3:0000:1319:8a2e:0370:7344', true ],
-    [ '2001:db8::1428:57ab' , true],
-    [ '2001:db8:::::1428:57ab', true],
+    ['2001:0db8:85a3:0000:1319:8a2e:0370:7344', true],
+    ['2001:db8::1428:57ab', true],
+    ['2001:db8:::::1428:57ab', true],
     ['::1', true],
-    [ '2001:db8::1428:57zy' , false],
+    ['2001:db8::1428:57zy', false],
 ]);
 
-test('that the attribute is return in the error message', function() {
+test('that the attribute is return in the error message', function () {
     $sut = new IPv6();
     expect(
         $sut->message()
