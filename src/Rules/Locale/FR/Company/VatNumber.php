f8beaa15fc032d9917e2c13a61d4705ec81a69aa
Sander van Kasteel <info@sandervankasteel.nl>
2022-01-01 21:13:43 +0100

    Added French VAT number validation

---
+++ src/Rules/Locale/FR/Company/VatNumber.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace LaravelExtendedValidation\Rules\Locale\FR\Company;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Str;
+
+class VatNumber implements Rule
+{
+    /**
+     * @inheritDoc
+     */
+    public function passes($attribute, $value)
+    {
+        /** @var string $numbers */
+        $numbers = Str::of($value)
+            ->replace(' ', '')
+            ->upper()
+            ->split('/FR/')
+            ->filter()
+            ->first();
+
+        if (! Str::of($value)->upper()->startsWith('FR') || Str::of($numbers)->length() !== 11) {
+            return false;
+        }
+
+        $siren = substr($numbers, 2);
+
+        return (12 + 3 * bcmod($siren, '97')) == substr($numbers, 0, 2);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function message()
+    {
+        return ':attribute does not contain a valid French VAT number';
+    }
+}
