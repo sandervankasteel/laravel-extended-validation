f11b98fbd413ad0b764b3a47a89f5f8c57733661
Sander van Kasteel <info@sandervankasteel.nl>
2022-01-01 17:27:23 +0100

    Added Belgium VAT number

---
+++ docs/locale/BE/Company/VatNumber.md
@@ -0,0 +1,12 @@
+# VatNumber
+### `LaravelExtendedValidation\Rules\Locale\BE\Company\VatNumber`
+
+Determines if the supplied VAT number is a valid Belgium VAT number.
+
+A valid example is `BE0418905287`
+
+## Constructor argument(s)
+
+```php
+none
+```
---
+++ tests/Locale/BE/VatNumberTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use LaravelExtendedValidation\Rules\Locale\BE\Company\VatNumber;
+
+test('that we can successfully validate VAT numbers', function ($vatNumber, $expectedOutput) {
+    $sut = new VatNumber();
+
+    expect(
+        $sut->passes('some-attribute', $vatNumber)
+    )->toBe($expectedOutput);
+})->with([
+    ['BE0809.387.596', true],
+    ['BE0809387596', true],
+    ['BE0418905287', true],
+    ['BE0999999923', false],
+]);
+
+test('that we get the attribute back from the error message', function () {
+    $sut = new VatNumber();
+    expect(
+        $sut->message()
+    )->toContain(':attribute');
+});
