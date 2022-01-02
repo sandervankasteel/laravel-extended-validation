d91f0e4166e78ca2a4612706378329e022981d6f
Sander van Kasteel <info@sandervankasteel.nl>
2022-01-02 12:30:37 +0100

    Added French postal code validation rule

---
+++ tests/Locale/FR/PostalCodeTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use LaravelExtendedValidation\Rules\Locale\FR\Address\PostalCode;
+
+test('that we can successfully validate a French postal code', function ($postalCode, $expectedResult) {
+    $sut = new PostalCode();
+
+    expect(
+        $sut->passes('some-attribute', $postalCode)
+    )->toBe($expectedResult);
+})->with([
+    ['64205', true],
+    ['64200', true],
+    ['00100', true], // Military
+    ['96000', false],
+    ['1234', false],
+    ['123456', false],
+]);
+
+test('that we can successfully validate an Monacan postal code', function ($postalCode, $expectedResult) {
+    $sut = new PostalCode(true);
+
+    expect(
+        $sut->passes('some-attribute', $postalCode)
+    )->toBe($expectedResult);
+})->with([
+    ['98000', true],
+]);
+
+test('that we get the attribute back from the error message', function () {
+    $sut = new PostalCode();
+
+    expect(
+        $sut->message()
+    )->toContain(':attribute');
+});
