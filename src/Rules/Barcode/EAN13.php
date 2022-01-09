be4262cdd39e8668c61394f4cc5641c65f0f39dd
Sander van Kasteel <info@sandervankasteel.nl>
2022-01-09 11:35:48 +0100

    Added docs for EAN-13 validation rule

---
+++ src/Rules/Barcode/EAN13.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace LaravelExtendedValidation\Rules\Barcode;
+
+class EAN13 implements \Illuminate\Contracts\Validation\Rule
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function passes($attribute, $value)
+    {
+        // TODO: Implement passes() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function message()
+    {
+        // TODO: Implement message() method.
+    }
+}
