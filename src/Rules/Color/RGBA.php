00d51c74666781f18c160256d2dd7b1d6477aa5d
Sander van Kasteel <info@sandervankasteel.nl>
2022-01-14 22:08:56 +0100

    Fixed return type on message

--- src/Rules/Color/RGBA.php
+++ src/Rules/Color/RGBA.php
@@ -16,7 +16,7 @@ class RGBA implements Rule
             ->replace(' ', '')
             ->lower();
 
-        if ($rgb->length() <= 11 || ! $rgb->length() > 17) {
+        if ($rgb->length() <= 11 || $rgb->length() > 17) {
             return false;
         }
 
@@ -56,7 +56,7 @@ class RGBA implements Rule
     /**
      * @inheritDoc
      */
-    public function message()
+    public function message(): string
     {
         return ':attribute does not contain a valid RGBA color';
     }
