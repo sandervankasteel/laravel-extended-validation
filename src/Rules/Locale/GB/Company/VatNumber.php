f2e552656d91c8978beaf0a7447dcb23ef9fe211
Sander van Kasteel <info@sandervankasteel.nl>
2022-12-12 22:05:42 +0100

    Added basic GB VAT number validation

---
+++ src/Rules/Locale/GB/Company/VatNumber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace LaravelExtendedValidation\Rules\Locale\GB\Company;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Str;
+
+class VatNumber implements Rule
+{
+    /**
+     * @inheritDoc
+     */
+    public function passes($attribute, $value): bool
+    {
+        /** @var string $numbers */
+        $numbers = Str::of($value)
+            ->replace(' ', '')
+            ->upper()
+            ->split('/GB/')
+            ->filter()
+            ->first();
+
+        if (!Str::of($value)->upper()->startsWith('GB')) {
+            return false;
+        }
+
+        if(Str::of($numbers)->length() !== 9 && Str::of($numbers)->length() !== 12) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function message(): string
+    {
+        return ':attribute does not contain a valid British VAT number';
+    }
+}
