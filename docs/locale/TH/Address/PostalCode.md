b8c50bdaaaebe75b5e05939b2822ec41573e9e92
Sander van Kasteel <info@sandervankasteel.nl>
2023-01-22 21:53:48 +0100

    Added docs for Thai Postalcode validation Rule

---
+++ docs/locale/TH/Address/PostalCode.md
@@ -0,0 +1,16 @@
+# Postal code
+
+Determines if the supplied postal code is conforming to the Thai standard of postal codes.
+
+- 43120
+- 20700
+- 38123
+
+
+### `LaravelExtendedValidation\Rules\Locale\TH\Address\PostalCode`
+
+### Constructor argument(s)
+
+```php
+none
+```
---
+++ src/Rules/Locale/TH/Address/PostalCode.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace LaravelExtendedValidation\Rules\Locale\TH\Address;
+
+class PostalCode implements \Illuminate\Contracts\Validation\Rule
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function passes($attribute, $value)
+    {
+        // TODO: Implement passes() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function message()
+    {
+        // TODO: Implement message() method.
+    }
+}
